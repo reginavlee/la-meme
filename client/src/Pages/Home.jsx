366113a6ff19d4e1a40df3d82ea43b22581109be
Jahosh <josh91hickman@gmail.com>
2017-03-11 11:16:57 -0800

    moved socket.io client to memeRoom

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,9 +1,5 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
-import { Button } from 'react-bootstrap';
-import io from 'socket.io-client';
-
-const socket = io('http://localhost:3000');
 
 class Home extends Component {
   constructor(props) {
@@ -13,18 +9,12 @@ class Home extends Component {
     };
   }
   componentDidMount() {
-    console.log(socket);
-  }
-  createRoom() {
-    const room = 'testRoom';
-    socket.emit('newRoom', room);
   }
   render() {
     return (
       <div>
         <h1>la même</h1>
         <Link to="dashboard">This takes you to dashboard page</Link>
-        <Button bsStyle="primary" onClick={this.createRoom}> Click</Button>
       </div>
     );
   }
--- client/src/Pages/MemeRoom.jsx
+++ client/src/Pages/MemeRoom.jsx
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import io from 'socket.io-client';
+import { Button } from 'react-bootstrap';
+
+const socket = io('http://localhost:3000');
 
 class Game extends Component {
   constructor(props) {
@@ -7,11 +11,21 @@ class Game extends Component {
     this.state = {
     };
   }
+  componentDidMount() {
+  }
+  createRoom() {
+    const user = {
+      name: 'Jahosh',
+      _id: '1',
+    };
+    socket.emit('chat-message', user);
+  }
   render() {
     return (
       <div>
         <h1>Game Stuff here!</h1>
         <Link to="/">This takes you to home page</Link>
+        <Button bsStyle="primary" onClick={this.createRoom}> Click</Button>
       </div>
     );
   }
