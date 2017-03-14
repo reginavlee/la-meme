e0b692bcaf348fff56ea5561cd426b475072eaee
Jahosh <josh91hickman@gmail.com>
2017-03-13 22:56:41 -0700

    added comments to logic, code clean-up

--- client/src/Pages/MemeRoomContainer.jsx
+++ client/src/Pages/MemeRoomContainer.jsx
@@ -28,21 +28,13 @@ class Game extends Component {
     window.onbeforeunload = () => {
       this.removeUser();
     };
-    // this.listenforCountdown();
   }
   /**
    * fire off socket connection on component mounting
    */
   componentDidMount() {
-    /* initialize client socket connection */
-    // this.socket = io('http://localhost:3000');
-    // this.createUser();
-    // this.createRoom();
-    // this.renderMessage();
-    // this.RoomOccupancy();
-    // this.triggerCountDown();
     this.listenforCountdown();
-    this.listenForPlayerStatus();
+    this.listenForConnectionType();
     this.listenForIntermission();
     this.roundOver();
   }
@@ -68,6 +60,9 @@ class Game extends Component {
     };
     this.socket.emit('create-user', payload);
   }
+  /**
+   * removes a user on the server
+   */
   removeUser() {
     const room = this.state.currentRoom;
     const user = this.state.username;
@@ -81,6 +76,9 @@ class Game extends Component {
     };
     this.socket.emit('forceDisconnect', payload);
   }
+  /**
+   * creates a room on the server to hold sockets
+   */
   createRoom() {
     const self = this;
     this.socket.emit('create-room', 'testRoom');
@@ -90,21 +88,27 @@ class Game extends Component {
       });
     });
   }
+  /**
+   * triggers the server to start the countdown
+   */
   triggerCountDown() {
-    console.log('trigger fired', this.state);
-    // state is static if called from component did componentDidMount
-    // this should be called from componentDid Update or something ..
     if (!this.state.countingDown && this.state.playerCount === 2 && this.state.connectionType !== 'spectator') {
       this.socket.emit('start-round', 'testRoom');
     }
   }
-  listenForPlayerStatus() {
+  /**
+   * listens for server's assignment of connectionType
+   */
+  listenForConnectionType() {
     this.socket.on('status', (connectionType) => {
       this.setState({
         connectionType
       });
     });
   }
+  /**
+   * listens for the time that is emitted from server ( every second )
+   */
   listenforCountdown() {
     const self = this;
     this.socket.on('count-down', ({ time, countingDown }) => {
@@ -114,27 +118,38 @@ class Game extends Component {
       });
     });
   }
+  /**
+   * round is over, updates round count
+   */
   roundOver() {
     const self = this;
     this.socket.on('round-over', (round) => {
       console.log(`round ${round} is over!`);
       const count = round + 1;
       this.showMeme();
-      // document.getElementById('display-meme').className = 'meme-display';
       self.setState({
         countingDown: false,
         round: count
       });
     });
   }
+  /**
+   * shows both players memes to everyone
+   */
   showMeme() {
     console.log('should show meme');
     document.getElementById('display-meme').removeAttribute('class');
   }
+  /**
+   * hides both players memes to everyone
+   */
   hideMeme() {
     console.log('should hide meme');
     document.getElementById('display-meme').className = 'meme-display';
   }
+  /**
+   * listens for intermission & game-over from the server countdown
+   */
   listenForIntermission() {
     const self = this;
     this.socket.on('intermission', () => {
@@ -154,6 +169,9 @@ class Game extends Component {
       });
     });
   }
+  /**
+   * listens for room occupancy changes from the server
+   */
   RoomOccupancy() {
     this.socket.on('occupancy', ({ playerCount, spectatorCount }) => {
       this.setState({
@@ -162,6 +180,9 @@ class Game extends Component {
       });
     });
   }
+  /**
+   * handles sending message through socket.io ( not used as of now, maybe chat later? )
+   */
   emitMessage(message) {
     const user = this.state.username;
     const room = this.state.currentRoom;
@@ -172,6 +193,9 @@ class Game extends Component {
     };
     this.socket.emit('chat-message', payload);
   }
+  /**
+   * handles rendering message to all clients ( not used as of now, maybe chat later? )
+   */
   renderMessage() {
     this.socket.on('new-message', (data) => {
       console.log('from renderMessage', data);
