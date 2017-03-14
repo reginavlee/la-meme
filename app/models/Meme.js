44c6891b81fcdc382807e9a4f860d990d531c8ea
iawen28 <marcie.ellen.anderson@gmail.com>
2017-03-13 21:00:03 -0700

    Meme

--- app/models/Meme.js
+++ app/models/Meme.js
@@ -2,9 +2,7 @@ const Sequelize = require('sequelize');
 const db = require('../db/db');
 
 const Meme = db.define('Meme', {
-  href: Sequelize.STRING,
-  text: Sequelize.STRING,
-  author: Sequelize.STRING //will probably need to change this to a user id
+  href: Sequelize.STRING
 });
 
 module.exports = Meme;
