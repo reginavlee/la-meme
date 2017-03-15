c567cc5d5e091ed03ee09da9472ecf9ac754ca11
iawen28 <marcie.ellen.anderson@gmail.com>
2017-03-15 16:11:03 -0700

    can probably delete whole models folder

--- app/models/Room.js
+++ app/models/Room.js
@@ -1,9 +1,9 @@
-const Sequelize = require('sequelize');
-const db = require('../db/db');
+// const Sequelize = require('sequelize');
+// const db = require('../db/db');
 
-const Room = db.define('Room', {
-  roomName: Sequelize.STRING,
-  clients: Sequelize.STRING,
-});
+// const Room = db.define('Room', {
+//   roomName: Sequelize.STRING,
+//   clients: Sequelize.STRING,
+// });
 
-module.exports = Room;
+// module.exports = Room;
--- app/models/User.js
+++ app/models/User.js
@@ -1,8 +1,9 @@
-const Sequelize = require('sequelize');
-const db = require('../db/db');
+// const Sequelize = require('sequelize');
+// const db = require('../db/db');
 
-const User = db.define('User', {
-  auth0Id: Sequelize.STRING,
-});
 
-module.exports = User;
+// const User = db.define('User', {
+//   auth0Id: Sequelize.STRING,
+// });
+
+// module.exports = User;
