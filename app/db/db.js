48d6f08f57f9c0d6dd74b5fe0b848410774c726c
iawen28 <marcie.ellen.anderson@gmail.com>
2017-03-15 16:09:00 -0700

    configured database

--- app/db/db.js
+++ app/db/db.js
@@ -1,13 +1,40 @@
 const Sequelize = require('sequelize');
+// const User = require('../models/User');
+// const Meme = require('../models/Meme');
+// const Room = require('../models/Room');
 
-const db = new Sequelize('lameme', 'root', 'lameme', {
-  host: 'localhost',
-  dialect: 'mysql',
-  pool: {
-    max: 10,
-    min: 0,
-    idle: 10000,
-  },
+const db = new Sequelize('postgres://avehvksm:BsxxRvnB0sMhTN48xMclrogYVhcmLlxY@stampy.db.elephantsql.com:5432/avehvksm');
+
+
+const Meme = db.define('Memes', {
+  href: {type: Sequelize.STRING}
 });
 
-module.exports = db;
+const User = db.define('User', {
+  auth0Id: Sequelize.STRING,
+});
+
+const Room = db.define('Room', {
+  roomName: Sequelize.STRING,
+  clients: Sequelize.STRING,
+});
+
+User.sync();
+Meme.sync();
+Room.sync();
+
+db.authenticate()
+    .then((err) => {
+        console.log('Successful Connection to the database');
+    })
+    .catch((err) => {
+        console.log('Cannot connect to the database due to:', err);
+    });
+
+module.exports = {
+  db: db,
+  Meme: Meme,
+  User: User,
+  Room: Room
+};
+
