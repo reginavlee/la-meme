565661ad261dce2291a2ac29ecdcdea0d45639cc
iawen28 <marcie.ellen.anderson@gmail.com>
2017-03-15 16:21:21 -0700

    style

--- app/db/db.js
+++ app/db/db.js
@@ -7,7 +7,7 @@ const db = new Sequelize('postgres://avehvksm:BsxxRvnB0sMhTN48xMclrogYVhcmLlxY@s
 
 
 const Meme = db.define('Memes', {
-  href: {type: Sequelize.STRING}
+  href: { type: Sequelize.STRING }
 });
 
 const User = db.define('User', {
@@ -24,11 +24,11 @@ Meme.sync();
 Room.sync();
 
 db.authenticate()
-    .then((err) => {
-        console.log('Successful Connection to the database');
+    .then(() => {
+      console.log('Successful Connection to the database');
     })
     .catch((err) => {
-        console.log('Cannot connect to the database due to:', err);
+      console.log('Cannot connect to the database due to:', err);
     });
 
 module.exports = {
