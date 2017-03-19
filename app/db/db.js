c68188998426209732f73f7ba227eff9a2e32f12
Jahosh <josh91hickman@gmail.com>
2017-03-19 01:43:03 -0700

    tweaks

--- app/db/db.js
+++ app/db/db.js
@@ -4,7 +4,11 @@ const Sequelize = require('sequelize');
 
 // insert your url below based on how your connecting to postgres //
 
+<<<<<<< HEAD
 const db = new Sequelize('postgres://avehvksm:BsxxRvnB0sMhTN48xMclrogYVhcmLlxY@stampy.db.elephantsql.com:5432/avehvksm');
+=======
+// const db = new Sequelize('postgres://avehvksm:BsxxRvnB0sMhTN48xMclrogYVhcmLlxY@stampy.db.elephantsql.com:5432/avehvksm');
+>>>>>>> tweaks
 
 // const db = new Sequelize('lameme', 'Jahosh', '', {
 //   host: 'localhost',
