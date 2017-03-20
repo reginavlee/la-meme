177322235d0973338c1315fbc48761073fe6aff5
iawen28 <marcie.ellen.anderson@gmail.com>
2017-03-20 10:09:41 -0700

    adjust no

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -18,7 +18,7 @@ module.exports = {
   memes: {
     // finds a random url from database
     get: (req, res) => {
-      const x = Math.ceil(Math.random() * 47);
+      const x = Math.ceil(Math.random() * 5);
       Memes.findAll({
         where: { id: x }
       })
