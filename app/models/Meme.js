7ddfa0a3b2a0d5b592c0cddb6db081a04c98f82f
Regina Lee <reginalee@Reginas-MacBook-Pro.local>
2017-03-23 11:10:52 -0700

    removed now unncessary files

--- app/models/Meme.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require('sequelize');
-const db = require('../db/db');
-
-const Memes = db.define('Memes', {
-  href: Sequelize.STRING
-});
-Memes.sync();
-
-module.exports = Memes;
--- app/models/Room.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require('sequelize');
-const db = require('../db/db');
-
-const Rooms = db.define('Rooms', {
-  name: Sequelize.STRING,
-});
-Rooms.sync();
-
-module.exports = Rooms;
