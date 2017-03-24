454da14510f3442f9c333eb45e851116f4ebc52b
Armen Rostamian <armenr@gmail.com>
2017-03-24 16:30:38 -0700

    we got working upload logic on the controllers/routes in express

--- routes.js
+++ routes.js
@@ -1,6 +1,36 @@
+const multer = require('multer');
+const AWS = require('aws-sdk');
 const router = require('express').Router();
 const cors = require('cors');
 const controller = require('./app/controllers/index');
+
+const s3 = new AWS.S3();
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  subregion: 'us-west-2',
+});
+
+const upload = multer({
+  storage: multer.memoryStorage(),
+  // file size limitation in bytes
+  limits: {
+    fileSize: 52428800
+  },
+});
+
+router.post('/upload', upload.single('theseNamesMustMatch'), (req, res) => {
+  // req.file is the 'theseNamesMustMatch' file
+  s3.putObject({
+    Bucket: 'lameme1',
+    Key: 'uploads',
+    Body: req.file.buffer,
+    ACL: 'public-read', // your permisions  
+  }, (err) => {
+    if (err) return res.status(400).send(err);
+    res.send('File uploaded to S3');
+  })
+})
 
 /*
  * {Client} Routes
@@ -26,4 +56,4 @@ router.get('/api/rooms', controller.rooms.get);
 router.post('/api/rooms', controller.rooms.post);
 router.get('api/rooms/:roomId', controller.rooms.getById);
 
-module.exports = router;
+module.exports = router
